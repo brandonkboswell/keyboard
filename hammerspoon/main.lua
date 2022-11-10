e31a5f286c5fd158ff80971196e84513aacf9a98
Brandon Boswell <brandonkboswell@gmail.com>
2022-11-10 17:53:45 -0500

    Migrated to Yabai

--- hammerspoon/main.lua
+++ hammerspoon/main.lua
@@ -29,41 +29,156 @@ toggleFocus = function(name, altName)
   end
 end
 
--- hs.hotkey.bind({'ctrl'}, 'space', function()
---   toggleFocus('Obsidian')
+--
+-- /opt/homebrew/bin/YABAI SHORTCUTS
+--
+
+-- Layout
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'f4', function()
+  os.execute("/opt/homebrew/bin/yabai -m space --layout stack")
+end)
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '0', function()
+  os.execute("/opt/homebrew/bin/yabai -m space --layout stack")
+end)
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '9', function()
+  os.execute("/opt/homebrew/bin/yabai -m space --layout bsp")
+end)
+
+-- focus window
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 's', function()
+  -- os.execute("/opt/homebrew/bin/yabai -m window --focus next || /opt/homebrew/bin/yabai -m window --focus first")
+  output = hs.execute("/opt/homebrew/bin/yabai -m query --spaces --space | /opt/homebrew/bin/jq -r '.type'")
+
+  print(output)
+
+  if output == "stack\n" then
+    os.execute("/opt/homebrew/bin/yabai -m window --focus stack.next || /opt/homebrew/bin/yabai -m window --focus stack.first")
+  else
+    os.execute("/opt/homebrew/bin/yabai -m window --focus next || /opt/homebrew/bin/yabai -m window --focus first")
+  end
+end)
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'd', function()
+  -- os.execute("/opt/homebrew/bin/yabai -m window --focus prev || /opt/homebrew/bin/yabai -m window --focus last")
+  output = hs.execute("/opt/homebrew/bin/yabai -m query --spaces --space | /opt/homebrew/bin/jq -r '.type'")
+
+  if output == "stack\n" then
+    os.execute("/opt/homebrew/bin/yabai -m window --focus stack.prev || /opt/homebrew/bin/yabai -m window --focus stack.last")
+  else
+    os.execute("/opt/homebrew/bin/yabai -m window --focus prev || /opt/homebrew/bin/yabai -m window --focus last")
+  end
+end)
+
+-- hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'f', function()
+--   os.execute("/opt/homebrew/bin/yabai -m window --focus east")
+-- end)
+-- hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'a', function()
+--   os.execute("/opt/homebrew/bin/yabai -m window --focus west")
 -- end)
 
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'x', function()
+  os.execute("/opt/homebrew/bin/yabai -m window --swap south || /opt/homebrew/bin/yabai -m window --swap east || /opt/homebrew/bin/yabai -m window --swap north || /opt/homebrew/bin/yabai -m window --swap west")
+end)
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'c', function()
+  os.execute("/opt/homebrew/bin/yabai -m window --swap north || /opt/homebrew/bin/yabai -m window --swap west || /opt/homebrew/bin/yabai -m window --swap south || /opt/homebrew/bin/yabai -m window --swap east")
+end)
+
+-- Manipulate Window Size
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '-', function()
+  os.execute("/opt/homebrew/bin/yabai -m window --ratio rel:-0.05")
+end)
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '=', function()
+  os.execute("/opt/homebrew/bin/yabai -m window --ratio rel:0.05")
+end)
+
+-- Monitor
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'a', function()
+  os.execute("/opt/homebrew/bin/yabai -m display --focus prev || /opt/homebrew/bin/yabai -m display --focus last")
+end)
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'f', function()
+  os.execute("/opt/homebrew/bin/yabai -m display --focus next || /opt/homebrew/bin/yabai -m display --focus first")
+end)
+
+-- send window to monitor and follow focus
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'z', function()
+  os.execute("/opt/homebrew/bin/yabai -m window --display 2; /opt/homebrew/bin/yabai -m display --focus 2")
+  -- os.execute("/opt/homebrew/bin/yabai -m display --focus prev || /opt/homebrew/bin/yabai -m display --focus last; /opt/homebrew/bin/yabai -m window --display prev || /opt/homebrew/bin/yabai -m display --focus last")
+end)
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'v', function()
+  -- os.execute("/opt/homebrew/bin/yabai -m window --display 1; /opt/homebrew/bin/yabai -m display --focus 1")
+  os.execute("/opt/homebrew/bin/yabai -m window --display next || /opt/homebrew/bin/yabai -m window --display first; /opt/homebrew/bin/yabai -m display --focus next || /opt/homebrew/bin/yabai -m display --focus first")
+end)
+
+-- rotate tree
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'e', function()
+  os.execute("/opt/homebrew/bin/yabai -m space --rotate 90")
+end)
+
+-- toggle window fullscreen zoom
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'return', function()
+  -- os.execute("/opt/homebrew/bin/yabai -m window --toggle zoom-fullscreen")
+  output = hs.execute("/opt/homebrew/bin/yabai -m query --spaces --space | /opt/homebrew/bin/jq -r '.type'")
+
+  if output == "stack\n" then
+    os.execute("/opt/homebrew/bin/yabai -m space --layout bsp;")
+    -- os.execute("/opt/homebrew/bin/yabai -m space --layout bsp; /opt/homebrew/bin/yabai -m space --padding abs:15:15:15:15; /opt/homebrew/bin/yabai -m space --gap abs:15")
+  else
+    os.execute("/opt/homebrew/bin/yabai -m space --layout stack;")
+    -- os.execute("/opt/homebrew/bin/yabai -m space --layout stack; /opt/homebrew/bin/yabai -m space --padding abs:0:0:0:0; /opt/homebrew/bin/yabai -m space --gap abs:0")
+  end
+end)
+
+-- toggle padding and gap
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'g', function()
+  os.execute("/opt/homebrew/bin/yabai -m space --toggle padding; /opt/homebrew/bin/yabai -m space --toggle gap")
+end)
+
+-- float / unfloat window and center on screen
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '\\', function()
+  os.execute("/opt/homebrew/bin/yabai -m window --toggle float; /opt/homebrew/bin/yabai -m window --grid 12:12:2:2:8:8")
+end)
+
+-- balance size of windows
+hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'r', function()
+  os.execute("/opt/homebrew/bin/yabai -m space --balance")
+end)
+
+--
+-- Shortcuts
+--
 
 hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '1', function()
-  hs.execute("open https://mail.google.com/mail/u/0/#inbox")
+  os.execute("open https://mail.google.com/mail/u/0/#inbox")
 end)
 
 hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '2', function()
-  hs.execute("open https://mail.google.com/mail/u/1/#inbox")
+  os.execute("open https://mail.google.com/mail/u/1/#inbox")
 end)
 
 hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '3', function()
-  hs.execute("open https://www.notion.so/dorsata/ac4230c7df3848118c54269308493230?v=5713ab04faa440d5b1314dd8bf4fc39f")
+  os.execute("open https://www.notion.so/dorsata/ac4230c7df3848118c54269308493230?v=5713ab04faa440d5b1314dd8bf4fc39f")
 end)
 
 hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '4', function()
-  hs.execute("open https://dorsata.atlassian.net/jira/software/c/projects/DOR/boards/2?quickFilter=3")
+  os.execute("open https://dorsata.atlassian.net/jira/software/c/projects/DOR/boards/2?quickFilter=3")
 end)
 
 hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '5', function()
-  hs.execute("open https://calendar.google.com/calendar/u/0/r")
+  os.execute("open https://calendar.google.com/calendar/u/0/r")
 end)
 
 hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, '6', function()
-  hs.execute("open https://www.figma.com/files/recent")
-end)
-
-hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 'space', function()
-  toggleFocus('Akiflow')
+  os.execute("open https://www.figma.com/files/recent")
 end)
 
 hs.hotkey.bind({'alt', 'ctrl', 'cmd', 'shift'}, 't', function()
-  toggleFocus('Warp')
+  toggleFocus('iTerm2')
 end)
 
 -- Use option + h to delete previous word
